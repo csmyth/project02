4af1fec717dfbbdca5516a9fcabd6838c05ebbd0
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-01-24 18:17:04 -0700

    Level 6 player.rb file, incomplete, @ dies

--- rubywarrior/courtney-beginner/player.rb
+++ rubywarrior/courtney-beginner/player.rb
@@ -1,9 +1,27 @@
 class Player
   def play_turn(warrior)
-	if @health == nil
-		@health = warrior.health
+	if @prev_health == nil
+		@prev_health = warrior.health
+		@wall_found = false
 	end
-	if warrior.feel.empty? && warrior.health < @health
+	
+	if warrior.feel(:backward).wall?
+		@wall_found = true
+	end
+	
+	
+	if warrior.feel(:backward).empty? && @wall_found != true 
+		warrior.walk!(:backward)
+		
+	elsif warrior.feel(:backward).captive?
+			warrior.rescue!(:backward)
+	elsif warrior.feel.captive?
+		warrior.rescue!
+	
+	elsif warrior.feel(:backward).enemy?
+			warrior.attack!(:backward)
+	
+	elsif warrior.feel.empty? && warrior.health < @prev_health
 		warrior.walk!
 	elsif warrior.feel.empty?
 		if warrior.health < 20
@@ -11,11 +29,9 @@ class Player
 		else
 			warrior.walk!
 		end
-	elsif warrior.feel.captive?
-		warrior.rescue!
 	else
 		warrior.attack!
 	end
-	@health = warrior.health
+	@prev_health = warrior.health
   end
 end
