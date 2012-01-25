b08859b3b3e93fcb92dc468e4ba45ad9788bd51a
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-01-24 18:36:42 -0700

    Level 6 player.rb file, @ still dies, but code is closer

--- rubywarrior/courtney-beginner/player.rb
+++ rubywarrior/courtney-beginner/player.rb
@@ -1,28 +1,49 @@
 class Player
   def play_turn(warrior)
+	#Initialize instance variables
 	if @prev_health == nil
 		@prev_health = warrior.health
 		@wall_found = false
+		@close_enemy = true
+		@need_rest = false
 	end
 	
+	#Senses Evaluation
 	if warrior.feel(:backward).wall?
 		@wall_found = true
 	end
+	if warrior.feel.empty? && warrior.health < @prev_health
+		@close_enemy = false
+	else
+		@close_enemy = true
+	end
 	
-	
-	if warrior.feel(:backward).empty? && @wall_found != true 
+	#Conditional Actions
+	if @need_rest
+		if warrior.health < 20
+			warrior.rest!
+		else 
+			@need_rest = false
+			warrior.walk!
+		end
+		
+	elsif warrior.feel(:backward).empty? && !(@wall_found)
 		warrior.walk!(:backward)
 		
 	elsif warrior.feel(:backward).captive?
-			warrior.rescue!(:backward)
+		warrior.rescue!(:backward)
 	elsif warrior.feel.captive?
 		warrior.rescue!
 	
 	elsif warrior.feel(:backward).enemy?
-			warrior.attack!(:backward)
+		warrior.attack!(:backward)
 	
-	elsif warrior.feel.empty? && warrior.health < @prev_health
+	elsif !(@close_enemy)
 		warrior.walk!
+	elsif @close_enemy && warrior.health < 15
+		@need_rest = true
+		warrior.walk!(:backward)
+	
 	elsif warrior.feel.empty?
 		if warrior.health < 20
 			warrior.rest!
@@ -32,6 +53,8 @@ class Player
 	else
 		warrior.attack!
 	end
+	
+	#Record health
 	@prev_health = warrior.health
   end
 end
